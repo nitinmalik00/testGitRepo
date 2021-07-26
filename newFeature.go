793078f496c5fe0cb592e72bcc8b9fe99769bee5
Nitin Malik <nitinmalik02015@gmail.com>
2021-07-26 23:04:54 +0530

    New Feature added newFeature file.go  Changes to be committed: 	new file:   newFeature.go

---
+++ newFeature.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Hello,")
+	fmt.Println("THis is for new Feature update!!!")
+	version := rand.Intn(10)
+	fmt.Println("THe version of this app is: ", version)
+}
